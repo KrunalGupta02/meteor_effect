326ae4abace73755feca12ab3878487c2a9c59a1
Krunal Gupta <guptakrunal02@gmail.com>
2023-08-28 00:12:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MeteorPreview from "./MeteorPreview";
+
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-black to-gray-900 h-screen">
+      <MeteorPreview />
+    </div>
+  );
+}
+
+export default App;
